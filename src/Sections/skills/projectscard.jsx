e176ad5f3eb3b720365390b89d19beb76d7078b6
ThePrincipalCodez <wisdomiwunwa2@gmail.com>
2024-07-31 19:06:00 +0000

    Modification of the projects card and elimination of unnecessary resources

--- src/Sections/skills/projectscard.jsx
+++ src/Sections/skills/projectscard.jsx
@@ -6,15 +6,15 @@ export default function ProjectCard(props) {
     <div className="cards">
       <div className="p-wrapper">
         <h3 className="title">{props.title}</h3>
-        <div className="card-images">
+        {/* <div className="card-images">
           <img src={props.image}
           height={200}
           width={400}
           alt="" 
           className="project-img"
           />
-          </div>
-        <p className="text">{props.description}</p>
+          </div> */}
+        {/* <p className="text">{props.description}</p> */}
       </div>
     </div>
   );
